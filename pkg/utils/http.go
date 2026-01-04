161327dfc7e4181d50144ccd40a0a4b1a6a38226
DevVictor19 <antoniovictor12@live.com>
2026-01-04 12:09:21 -0300

    feat: add log response error util

--- pkg/utils/http.go
+++ pkg/utils/http.go
@@ -1,8 +1,27 @@
 package utils
 
-import "github.com/labstack/echo/v4"
+import (
+	"servicetemplate/pkg/logger"
+
+	"github.com/labstack/echo/v4"
+)
 
 // Get request id from echo context
 func GetRequestID(c echo.Context) string {
 	return c.Response().Header().Get(echo.HeaderXRequestID)
+}
+
+// Get user ip address
+func GetIPAddress(c echo.Context) string {
+	return c.Request().RemoteAddr
+}
+
+// Error response with logging error for echo context
+func LogResponseError(ctx echo.Context, logger logger.Logger, err error) {
+	logger.Errorf(
+		"ErrResponseWithLog, RequestID: %s, IPAddress: %s, Error: %s",
+		GetRequestID(ctx),
+		GetIPAddress(ctx),
+		err,
+	)
 }
